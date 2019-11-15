2dc19f3344aa1350b4fb7ff5ea5ba34c17841a78
Andrey <zakharenko.main@gmail.com>
2019-11-15 15:17:15 +0200

    add createElement method

--- src/createElement.js
+++ src/createElement.js
@@ -1,3 +1,25 @@
-export function createElement() {
-    return 3
+export function createElement(type, props, ...children) {
+    return {
+        type,
+        props: {
+            ...props,
+            children: children.map(child => {
+                return typeof child === "object"
+                    ? child
+                    : createTextElement(child);
+            }),
+        }
+    }
+}
+
+export const TEXT_ELEMENT_TYPE = 'TEXT_ELEMENT';
+
+function createTextElement(text) {
+    return {
+        type: 'TEXT_ELEMENT',
+        props: {
+            nodeValue: text,
+            children: [],
+        }
+    }
 }
--- test/createElement.test.js
+++ test/createElement.test.js
@@ -1,7 +1,49 @@
-import { createElement } from '../src/createElement'
+import { createElement, TEXT_ELEMENT_TYPE } from '../src/createElement'
 
 describe('createElement', () => {
-    it('adds 1 + 2 to equal 3', () => {
-        expect(createElement()).toBe(3);
+    it('should return recursively ', () => {
+        const element = createElement(
+            'div',
+            { className: 'wrapper' },
+            createElement('h1', null, 'title'),
+            createElement('p', null, 'description')
+        );
+
+        expect(element).toEqual({
+            type: 'div',
+            props: {
+                className: 'wrapper',
+                children: [
+                    {
+                        type: 'h1',
+                        props: {
+                            children: [
+                                {
+                                    type: TEXT_ELEMENT_TYPE,
+                                    props: {
+                                        nodeValue: 'title',
+                                        children: [],
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        type: 'p',
+                        props: {
+                            children: [
+                                {
+                                    type: TEXT_ELEMENT_TYPE,
+                                    props: {
+                                        nodeValue: 'description',
+                                        children: [],
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                ],
+            }
+        });
     });
 });
